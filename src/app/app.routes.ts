9f7a80d9d7b117216e3ea65e2b75f1ef2987bd06
Sam Buerck <samuel.buerck@jacks.sdstate.edu>
2024-12-28 20:29:19 -0600

    added view expense route and more styling

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { ExpenseListComponent } from './expense-list/expense-list.component';
 import { AddExpenseComponent } from './add-expense/add-expense.component';
 import { EditExpenseComponent } from './edit-expense/edit-expense.component';
+import { ViewExpenseComponent } from './view-expense/view-expense.component';
 
 export const routes: Routes = [
     { path: '', component: ExpenseListComponent, title: 'Budget Home' },
     { path: 'new', component: AddExpenseComponent, title: 'New Expense' },
     { path: 'edit/:id', component: EditExpenseComponent, title: 'Edit Expense' },
+    { path: 'expense/:id', component: ViewExpenseComponent, title: 'View Expense' },
 ];
