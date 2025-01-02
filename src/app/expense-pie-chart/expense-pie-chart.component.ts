1649ef7bccc3a813cb706d22ce045954f61674c4
Sam Buerck <samuel.buerck@jacks.sdstate.edu>
2025-01-01 22:58:39 -0600

    created pie chart component and installed charts.js

---
+++ src/app/expense-pie-chart/expense-pie-chart.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-expense-pie-chart',
+  imports: [],
+  template: `
+    <p>
+      expense-pie-chart works!
+    </p>
+  `,
+  styles: ``
+})
+export class ExpensePieChartComponent {
+
+}
