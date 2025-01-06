cb2ae985b29bd666cd94b38e5c6a00e9b987e6b7
Sam Buerck <samuel.buerck@jacks.sdstate.edu>
2025-01-05 23:30:33 -0600

    bit of formatting for chart

--- src/app/expense-pie-chart/expense-pie-chart.component.ts
+++ src/app/expense-pie-chart/expense-pie-chart.component.ts
@@ -1,13 +1,16 @@
 import { AfterViewInit, Component, OnInit, signal } from '@angular/core';
-import { ChartData, ChartType } from 'chart.js';
+import ChartDataLabels from 'chartjs-plugin-datalabels';
+import { Chart, registerables } from 'chart.js';
 import { BaseChartDirective } from 'ng2-charts';
 import { ExpenseListComponent } from '../expense-list/expense-list.component';
 import { Expense } from '../expense';
 import { ExpenseService } from '../expense.service';
 
+Chart.register(...registerables, ChartDataLabels);
+
 @Component({
   selector: 'app-expense-pie-chart',
-  imports: [BaseChartDirective],
+  imports: [BaseChartDirective,],
   templateUrl: `./expense-pie-chart.component.html`,
   styleUrl: `expense-pie-chart.component.css`
 })
@@ -15,13 +18,33 @@ export class ExpensePieChartComponent implements OnInit, AfterViewInit {
   expenses$ = signal<Expense[]>([]);
   expenseList: ExpenseListComponent;
 
+
   constructor(private expenseService: ExpenseService) { }
 
   public pieChartLabels = [];
   public pieChartDatasets = [{
     data: [300, 500, 100],
-    backgroundColor: ['#FF6384', '#36A2EB', '#FFCE56', '#4CAF50', '#FFC107'],
+    backgroundColor: ['#90A4AE', '#FFCC80', '#B0BEC5', '#FFE082', '#CFD8DC'],
   }];
+  public pieChartOptions = {
+    responsive: true,
+    plugins: {
+      legend: {
+        display: true, // Show legend
+      },
+      datalabels: {
+        color: '#000', // Label text color
+        formatter: (value: number, context: any) => {
+          const label = context.chart.data.labels[context.dataIndex];
+          return `${label}`; // Format the label
+        },
+        offset: (context: any) => {
+          const value = context.raw; // Get the data value for this slice
+          return value > 1000 ? 20 : 10; // Adjust offset based on value
+        },
+      },
+    },
+  };
 
   ngOnInit() {
     this.getExpenses();
