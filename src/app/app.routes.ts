5fb2dbcbcd5a8af9fb64deb56ce541ab421700e5
Sam Buerck <samuel.buerck@jacks.sdstate.edu>
2025-01-13 12:30:47 -0600

    Added login route.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { ExpenseListComponent } from './expense-list/expense-list.component';
 import { AddExpenseComponent } from './add-expense/add-expense.component';
 import { EditExpenseComponent } from './edit-expense/edit-expense.component';
 import { ViewExpenseComponent } from './view-expense/view-expense.component';
+import { LoginComponent } from './login/login.component';
 
 export const routes: Routes = [
     { path: '', component: ExpenseListComponent, title: 'Budget Home' },
     { path: 'new', component: AddExpenseComponent, title: 'New Expense' },
     { path: 'edit/:id', component: EditExpenseComponent, title: 'Edit Expense' },
     { path: 'expense/:id', component: ViewExpenseComponent, title: 'View Expense' },
+    { path: 'login', component: LoginComponent, title: 'Login' }
 ];
