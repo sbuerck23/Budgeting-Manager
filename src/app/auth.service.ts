a2af2117858eaf880c68e19ac44f50c01b68f5c9
Sam Buerck <samuel.buerck@jacks.sdstate.edu>
2025-01-13 15:16:02 -0600

    frontend for login/register

---
+++ src/app/auth.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  private url = 'http://localhost:5200';
+
+  constructor(private httpClient: HttpClient) { }
+
+  login(creds: { username: string, password: string }) {
+    this.httpClient.post(`${this.url}/login`, creds, { responseType: 'text' });
+  }
+
+  register(creds: { username: string, password: string }) {
+    this.httpClient.post(`${this.url}/register`, creds, { responseType: 'text' });
+  }
+}
---
+++ src/app/login/login.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { FormBuilder, Validators, ReactiveFormsModule, FormGroup } from '@angular/forms';
+import { AuthService } from '../auth.service';
+
+@Component({
+  selector: 'app-login',
+  imports: [ReactiveFormsModule],
+  templateUrl: `./login.component.html`,
+  styleUrl: `./login.component.css`
+})
+export class LoginComponent {
+  loginForm: FormGroup;
+  constructor(private formBuilder: FormBuilder, private authService: AuthService) {
+    this.loginForm = this.formBuilder.group({
+      username: ['', [Validators.required]],
+      password: ['', [Validators.required]],
+    });
+  }
+
+
+  onSubmit() {
+    console.log(this.loginForm.value);
+    this.authService.login(this.loginForm.value);
+  }
+}
