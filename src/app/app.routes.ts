e235e5fbecd69d668feeb0b0a2f157e22f6abaee
Sam Buerck <samuel.buerck@jacks.sdstate.edu>
2025-01-13 12:41:52 -0600

    list route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,7 +6,7 @@ import { ViewExpenseComponent } from './view-expense/view-expense.component';
 import { LoginComponent } from './login/login.component';
 
 export const routes: Routes = [
-    { path: '', component: ExpenseListComponent, title: 'Budget Home' },
+    { path: 'list', component: ExpenseListComponent, title: 'Budget Home' },
     { path: 'new', component: AddExpenseComponent, title: 'New Expense' },
     { path: 'edit/:id', component: EditExpenseComponent, title: 'Edit Expense' },
     { path: 'expense/:id', component: ViewExpenseComponent, title: 'View Expense' },
